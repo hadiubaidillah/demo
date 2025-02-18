cf01c4955a4790634cb38fe9aa7f29c824d50ddf
Hadi Ubaidillah <hadiubaidillahx@gmail.com>
2025-02-18 22:20:23 +0700

    update send-env-to-server.sh

--- send-env-to-server.sh
+++ send-env-to-server.sh
@@ -1,1 +1,3 @@
-scp -r ./backend/.env jenkins@server:/var/lib/jenkins/workspace/hadiubaidillah.com/backend/
+scp -r ./backend/.env jenkins@node0:/var/lib/jenkins/workspace/hadiubaidillah.com/backend/
+scp -r ./backend/.env jenkins-agent@node1:/home/jenkins-agent/workspace/hadiubaidillah.com/backend/
+scp -r ./backend/.env jenkins-agent@node2:/home/jenkins-agent/workspace/hadiubaidillah.com/backend/
